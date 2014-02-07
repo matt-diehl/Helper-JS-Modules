360dc26e78146c16478d5a0eb91e08698a427970
Matt Diehl <matt.diehl@berndtgroup.net>
2014-02-07 15:30:35 -0500

    Expand improvements - smooth transitions, less reliance on classes

--- Expand.js
+++ Expand.js
@@ -9,12 +9,14 @@ var Expand = (function (args) {
         settings: {
             expandToggle: $('.js-expand-toggle'), // Standard expand/collapse toggle
             expandOnlyToggle: $('.js-expand-only-toggle'), // Only expand, do not collapse
-            externalTrigger: $('.js-external-trigger') // Trigger another toggle
+            externalTrigger: $('.js-external-trigger'), // Trigger another toggle
+            expandRate: 3/5 // pixels/ms
         },
 
         init: function() {
             s = $.extend({}, this.settings, args);
             this.bindUIActions();
+            this.getSupportedProps();
         },
 
         // If the expand functionality only applies to one or two media queries, the data-expand-context attribute
@@ -37,6 +39,19 @@ var Expand = (function (args) {
             });
         },
 
+        getSupportedProp: function(proparray) {
+            var root = document.documentElement
+            for (var i = 0; i < proparray.length; i++){
+                if (typeof root.style[proparray[i]] == 'string'){
+                    return proparray[i]
+                }
+            }
+        },
+
+        getSupportedProps: function() {
+            s.cssTransition = this.getSupportedProp(['transition', 'MozTransition', 'WebkitTransition', 'msTransition', 'OTransition'])
+        },
+
         triggerActions: function(el, e) {
             var dataExpandItem = el.data('expand-item'),
                 dataCollapseItem = el.data('collapse'),
@@ -47,75 +62,100 @@ var Expand = (function (args) {
             e.stopPropagation();
             if (Media.meetsContext(context)) {
                 e.preventDefault();
-                if (collapseItem && collapseItem[0] !== expandItem[0]) {
+                if (collapseItem.length && collapseItem[0] !== expandItem[0]) {
                     Expand.collapse(collapseItem, collapseItem.closest('.js-expand-wrap'), context);
                 }
                 if (expandItem) {
                     Expand.toggle(expandItem, expandWrap, context);
                 }
-
             }
+        },
+
+        getClasses: function(context) {
+            var classes = ['', '', '', ''];
+            if (!context) {
+                context = [''];
+            }
+            for (var i = 0; i < context.length; i++) {
+                var suffix = context[i].length ? '-' + context[i] : '';
+                classes[0] += ' is-expanded' + suffix;
+                classes[1] += ' is-collapsed' + suffix;
+                classes[2] += ' module-is-expanded' + suffix;
+                classes[3] += ' module-is-collapsed' + suffix;
+            }
+            return classes;
+        },
+
+        getTransitionVal: function(height) {
+            var transitionTime = height / s.expandRate,
+                transitionVal = 'max-height ' + transitionTime + 'ms linear, padding ' + transitionTime + 'ms linear';
+            return transitionVal;
         },
 
         toggle: function(item, wrap, context) {
 
-            if (!context) {
-                context = [''];
-            }
-
-            for (var i = 0; i < context.length; i++) {
-
-                var suffix = context[i].length ? '-' + context[i] : '';
-
-                if (item) {
-                    item.toggleClass('is-expanded' + suffix).toggleClass('is-collapsed' + suffix);
-                }
-                if (wrap) {
-                    wrap.toggleClass('module-is-expanded' + suffix).toggleClass('module-is-collapsed' + suffix);
-                }
-
+            if (item[0].className.match(/is-collapsed/)) {
+                Expand.expand(item, wrap, context);
+            } else if (item[0].className.match(/is-expanded/)) {
+                Expand.collapse(item, wrap, context);
             }
 
         },
 
         expand: function(item, wrap, context) {
 
-            if (!context) {
-                context = [''];
+            var classes = Expand.getClasses(context),
+                expandHeight = item[0].scrollHeight,
+                transitionVal = Expand.getTransitionVal(expandHeight);
+
+            if (item.length) {
+                if (s.cssTransition.length) {
+                    item[0].style[s.cssTransition] = transitionVal;
+                    item[0].style.overflow = 'hidden';
+                    window.setTimeout(function() {
+                        item[0].style.maxHeight = expandHeight + 'px';
+                        item.removeClass(classes[1]).addClass(classes[0]);
+                        item.on('transitionend', function() {
+                            item.off('transitionend');
+                            item[0].removeAttribute('style');
+                        });
+                    }, 50);
+                } else { // If no transition support, just change classes
+                    item.removeClass(classes[1]).addClass(classes[0]);
+                }
             }
-
-            for (var i = 0; i < context.length; i++) {
-
-                var suffix = context[i].length ? '-' + context[i] : '';
-
-                if (item) {
-                    item.addClass('is-expanded' + suffix).removeClass('is-collapsed' + suffix);
-                }
-                if (wrap) {
-                    wrap.addClass('module-is-expanded' + suffix).removeClass('module-is-collapsed' + suffix);
-                }
-
+            if (wrap.length) {
+                wrap.addClass(classes[2]).removeClass(classes[3]);
             }
 
         },
 
         collapse: function(item, wrap, context) {
 
-            if (!context) {
-                context = [''];
+            var classes = Expand.getClasses(context),
+                expandHeight = item[0].scrollHeight,
+                transitionVal = Expand.getTransitionVal(expandHeight);
+
+            if (item) {
+                if (s.cssTransition.length) {
+                    item[0].style.maxHeight = expandHeight + 'px';
+                    item.removeClass(classes[0]);
+                    item[0].style[s.cssTransition] = transitionVal;
+                    item[0].style.overflow = 'hidden';
+                    window.setTimeout(function() {
+                        item[0].style.maxHeight = '0px';
+                        item.on('transitionend', function(a) {
+                            item.addClass(classes[1]);
+                            item[0].removeAttribute('style');
+                            item.off('transitionend');
+                        });
+                    }, 50);
+                } else {
+                    item.removeClass(classes[0]).addClass(classes[1]);
+                }
             }
-
-            for (var i = 0; i < context.length; i++) {
-
-                var suffix = context[i].length ? '-' + context[i] : '';
-
-                if (item) {
-                    item.removeClass('is-expanded' + suffix).addClass('is-collapsed' + suffix);
-                }
-                if (wrap) {
-                    wrap.removeClass('module-is-expanded' + suffix).addClass('module-is-collapsed' + suffix);
-                }
-
+            if (wrap) {
+                wrap.removeClass(classes[2]).addClass(classes[3]);
             }
 
         }
