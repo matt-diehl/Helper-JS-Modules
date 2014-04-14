194a32a80860bcce50e34ae3c07f3ea3b28d63f8
Matt Diehl <matt.diehl@berndtgroup.net>
2014-04-14 17:46:26 -0400

    prevent async on dependency loading when multiple dependencies are specified

--- Dependencies.js
+++ Dependencies.js
@@ -1,12 +1,12 @@
 /*global $ */
 
 // This function handles calling secondary scripts that are only needed on certain pages
-// The data-depend' attribute/value class is applied somewhere in the html of the page
+// The 'data-depend' attribute/value is applied somewhere in the html of the page
 // with the value being the name of the needed module/script. Multiple scripts can be used if divided by a vertical bar ('|')
 
 'use strict';
 
-var Dependencies = (function (args) {
+var Dependencies = (function () {
     var s;
 
     return {
@@ -17,29 +17,32 @@ var Dependencies = (function (args) {
         },
 
         init: function() {
-            s = $.extend({}, this.settings, args);
+            s = this.settings;
             this.getDependencies();
         },
 
         getDependencies: function() {
             if (s.dependToggles.length) {
                 s.dependToggles.each(function() {
-                    var dependArray = $(this).data('depend').split('|');
+                    var dependArray = $(this).data('depend').split('|'),
+                        // If there aren't multiple dependencies, we'll set it to load async
+                        // Otherwise we'll assume that each following script depends on the previous, and not load async
+                        async = dependArray.length > 1 ? false : true;
                     for (var i = 0; i < dependArray.length; i++) {
                         // Check if dependency has already been loaded first
                         if ($.inArray(dependArray[i], s.loadedDependencies) < 0) {
                             s.loadedDependencies.push(dependArray[i]);
-                            Dependencies.appendScript(dependArray[i]);
+                            Dependencies.appendScript(dependArray[i], async);
                         }
                     }
                 });
             }
         },
 
-        appendScript: function(module) {
+        appendScript: function(module, async) {
             var script = document.createElement('script');
             script.type = 'text/javascript';
-            script.async = true;
+            script.async = async;
             script.src = module;
             s.scriptsWrap.appendChild(script);
         }
@@ -47,3 +50,5 @@ var Dependencies = (function (args) {
     };
 
 })();
+
+Dependencies.init();
